5a0d6ba035591250209e6799eae43ef2e0b8fe98
Scott Atkins <satkins16@gmail.com>
2017-09-20 13:58:47 +0000

    Done.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -11,8 +11,14 @@ class Cart < ActiveRecord::Base
     return total
   end
 
-  def add_item
-
+  def add_item(item_id)
+    line_item = self.line_items.find_by(item_id: item_id)
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = self.line_items.build(item_id: item_id)
+    end
+    line_item
   end
 
 end
