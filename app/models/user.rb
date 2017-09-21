3f183f8966aae33b4b3c7b840421d5cd024d65f7
Scott Atkins <satkins16@gmail.com>
2017-09-21 00:48:25 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :carts
+  belongs_to :current_cart, class_name: "Cart"
+
+  def create_current_cart
+    new_cart = Cart.create
+    self.current_cart_id = new_cart.id
+    save
+  end
+
 end
