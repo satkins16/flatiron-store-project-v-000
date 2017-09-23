f1411e2eb820d2470cc05e54d37b7e0b4dda523c
Scott Atkins <satkins16@gmail.com>
2017-09-23 20:37:46 +0000

    Done.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,9 @@
 class StoreController < ApplicationController
 
   def index
+    # binding.pry
+    @items = Item.available_items.order(:title)
+    @categories = Category.order(:title)
   end
 
 end
